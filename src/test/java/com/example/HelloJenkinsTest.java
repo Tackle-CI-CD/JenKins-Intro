cd597dc566736017b46b8362d66bffe5fa94a2b8
saeed <ahmed.m.elsaeed2@gmail.com>
2024-02-19 10:20:44 +0200

    create new java project

---
+++ src/test/java/com/example/HelloJenkinsTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.junit.Test;
+
+public class HelloJenkinsTest {
+
+    @Test
+    public void verifyNoExceptionThrown() {
+        HelloJenkins.main(new String[]{});
+    }
+}
